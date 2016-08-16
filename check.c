b2b837d58d3605eac6621317cec07c72524205be
Maarten Pronk <git@evetion.nl>
2016-08-16 16:18:32 +0200

    First outline. File header working.

--- check.c
+++ check.c
@@ -1,10 +1,9 @@
+#include "sqlite/sqlite3.h"
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
-/* minimum required number of parameters */
-#define MIN_ARG 2
-#define MAX_ARG 2
 
 /* display help */
 int help() {
@@ -20,9 +19,23 @@ int help() {
 int main(int argc, char *argv[])
 {
     bool check = true;
-    int stream;
-    int header[1];
+    bool exists = false;
+    bool valid = false;
+
+    char *create_sql;
+    char *drop_sql;
+    char *insert_sql;
+    char *pragma_sql;
+    char *read_sql;
+
     FILE *sqldb;
+    int  rc;
+    sqlite3 *db;
+    uint32_t changes;
+
+    unsigned char header[100];
+    unsigned char wal_read;
+    unsigned char wal_write;
 
     /* check number of arguments */
     if (argc != 3) {
@@ -50,21 +63,70 @@ int main(int argc, char *argv[])
     }
     else
     {
+        /* read complete header */
+        fread(header, 1, 100, sqldb);
+
+        /*  file format write version.
+        1 for legacy; 2 for WAL. */
+        wal_read = header[18];
+        /*  file format write version.
+        1 for legacy; 2 for WAL. */
+        wal_write = header[19];
+
         /* header change value at 24..27 */
-        fseek(sqldb, 24, SEEK_SET);
-        fread(header, 4, 1, sqldb);
+        // fseek(sqldb, 24, SEEK_SET);
+        changes = header[27] | (header[26] << 8)  | (header[25] << 16) | (header[24] << 24);
+        fclose (sqldb);
     }
-    printf("header change value is at %i\n", *header);
-    //       {
-    //           n++;
+    printf("header change value is at %i\n", changes);
+    printf("wal read value is at %u\n", wal_read);
+    printf("wal write value is at %u\n", wal_write);
 
-    //           if(n >23 && n < 28)
-    //           {
-    //               printf("%x",c);    // printing the value of header whic in
-    //               i++;
-    //           }
-    //       }
-    fclose (sqldb);
+    /* Open database */
+    rc = sqlite3_open("test.db", &db);
+    if(rc){
+        printf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return 1;
+    } else {
+        /* Check existing meta table
+        so we know it exists and is correct
+        - table
+        - columns
+        - row */
+        exists = true;
+        
+        /* Check database */
+        if (check) {
+            if (valid && exists) {
+                if ((wal_read != 1) || (wal_write != 1)) {
+                    perror("Database changed to WAL mode.");
+                    return 1;
+                } else {
+                    // SQL READ
+                    // COMPARE
+                }
+            } else {
+                perror("Can't read table, database is not original.");
+                return 1;
+            }
 
+        /* Set meta table */
+        } else {
+            // SET PRAGMA           
+            if (!valid) {
+                // DROP TABLE
+                //
+                exists = false;
+            }
+            if (!exists) {
+                // CREATE TABLE
+                exists = true
+            }
+            // SET VALUES
+            // SQL INSERT
+            valid = true
+        }
+    }
+    sqlite3_close(db);
     return 0;
 }
